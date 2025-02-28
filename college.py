a5ac346ca32f97c71383642a33963beedb93b08d
nagasri-07 <yervanagasri@gmail.com>
2025-02-28 11:14:43 +0530

    Update college.py

--- college.py
+++ college.py
@@ -22,7 +22,7 @@ genai.configure(api_key=API_KEY)
 model=genai.GenerativeModel('gemini-1.5-flash')
 def find_closest_question(user_query,vectorizer,question_vectors,df):
   query_vector=vectorizer.transform({user_query.lower()})
-  similarities=cosine_similarity(query_vector,question_vectors.flatten()
+  similarities=cosine_similarity(query_vector,question_vectors.flatten())
   best_match_index=similarities.argmax()
   best_match_score=similarities[best_match_index]
   if best_match_score>0.3:
